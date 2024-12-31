7147e50d95a6fc7df9a015ba2761b72c004c888d
ja204 <jagajanardhan1@gmail.com>
2024-12-31 13:18:04 +0530

    Add project files.

---
+++ Models/CompanyContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCusingEFCoreDBFirst.Models;
+
+public partial class CompanyContext : DbContext
+{
+    public CompanyContext()
+    {
+    }
+
+    public CompanyContext(DbContextOptions<CompanyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC077ABC110D");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Department).HasMaxLength(50);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.Salary).HasColumnType("numeric(18, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
